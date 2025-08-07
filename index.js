98945d8aeabebd4d664eb4268f4f67294a7e6299
Paul <hehu2012@gmail.com>
2025-08-07 16:45:11 +0800

    Update index.js
    1

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ export default {
       const requestHeaders = new Headers(request.headers);
 
       // 确保不设置或覆盖 Host，也可选择删除 Host 头（目标服务器可能会自动处理）
-       requestHeaders.set("Host", "api.soulchill.live"); // ❌ 现在不设置
+       requestHeaders.set("Host", "sc-api.doki.ren"); // ❌ 现在不设置
       //requestHeaders.delete("Host"); // ✅ 推荐删除，防止冲突
 
       // 读取请求体（仅非 GET/HEAD）
